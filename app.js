81ea38d757da40e7664b1cb1b6b8744a56495da3
Vansh Rastogi <vanshrastogi0x01@gmail.com>
2026-01-28 16:43:06 +0530

    Handle errors consistently in all routes

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const Listing=require("./models/listing.js");
 const path=require("path");
 const methodOverride=require("method-override");
 const ejsMate=require("ejs-mate");
+const wrapAsync=require("./utils/wrapAsync.js");
+const ExpressError=require("./utils/ExpressError.js");
+const {listingSchema} = require("./schema.js");
 
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({extended:true}));
@@ -22,9 +25,16 @@ async function main() {
     await mongoose.connect('mongodb://127.0.0.1:27017/wanderlust');
 }
 
-app.listen(8080, ()=>{
-    console.log("Server is listening to port 8080");
-});
+const validateListing = (req,res,next)=>{
+    let {error}=listingSchema.validate(req.body);
+    if(error){
+        let errMsg=error.details.map((el)=>el.message).join(",");
+        throw new ExpressError(400, errMsg);
+    }
+    else{
+        next();
+    }
+}
 app.get("/",(req,res)=>{
     res.send("Route is working");
 });
@@ -37,30 +47,45 @@ app.get("/listings/new", async (req,res)=>{
     const listing= await Listing.findById(id);
     res.render("listings/new.ejs",{listing});
 });
-app.get("/listings/:id", async (req,res)=>{
+app.get("/listings/:id", wrapAsync(async (req,res)=>{
     let {id}=req.params;
     const listing= await Listing.findById(id);
     res.render("listings/show.ejs",{listing});
-});
+}));
 app.get("/listings/:id/edit", async (req,res)=>{
     let {id}=req.params;
     const listing= await Listing.findById(id);
     res.render("listings/edit.ejs",{listing});
 });
 
-app.post("/listings", async (req,res)=>{
+app.post("/listings", validateListing, wrapAsync(async (req,res,next)=>{
     const listing = new Listing (req.body.listing) ;
     await listing.save();
     res.redirect("/listings");
-});
-app.put("/listings/:id", async (req,res)=>{
+}));
+
+app.put("/listings/:id", validateListing, wrapAsync(async (req,res)=>{
     let {id}=req.params;
     await Listing.findByIdAndUpdate(id,{...req.body.listing});
     res.redirect(`/listings/${id}`);
-})
+}));
 app.delete("/listings/:id", async (req,res)=>{
     let {id}=req.params;
     let deletedListing= await Listing.findByIdAndDelete(id);
     console.log(deletedListing);
     res.redirect(`/listings`);
-})
+});
+
+// app.all("/*", (req,res,next)=>{
+//     next(new ExpressError(404, "Page not found"));
+// });
+
+app.use((err,req,res,next)=>{
+    let {statusCode=500, message="Something went wrong"}=err;
+    res.status(statusCode).render("error.ejs",{message})
+    // res.status(statusCode).send(message);
+});
+
+app.listen(8080, ()=>{
+    console.log("Server is listening to port 8080");
+});
